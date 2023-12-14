05b4f2c49036c18216ac4fbd88943c93f134e900
Neelam <neelammujawar12@gmail.com>
2023-12-14 12:37:16 +0530

    forth programm

--- Dog.java
+++ Dog.java
@@ -1,3 +1,36 @@
+import java.util.*;
 public class Dog {
-    
+String  breed;
+int age;
+double weigth;
+int HumanDogAge;
+Dog(String breed,int age,double weight){
+this.breed=breed;
+this.age=age;
+this.weigth=weight;
+}
+
+double HumanDogAge(){
+HumanDogAge=age*7;
+return HumanDogAge;
+
+}
+void PrintAge(){
+  System.out.println("dog's age in human years"+HumanDogAge);
+
+
+}
+public static void main(String[] args) {
+     Scanner dog= new Scanner(System.in);
+    System.out.println("enter breed");
+    String breed= dog.next();
+    System.out.println("enter age");
+     int age=dog.nextInt();
+    System.out.println("enter weight");
+     double weigth=dog.nextDouble();
+     Dog dg=new Dog(breed,age,weigth);
+     dg.HumanDogAge();
+     dg.PrintAge();
+   
+}
 }
---
+++ Phone.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Phone {
+    String model;
+    String storage;
+    String make;
+
+    public static void main(String[] args) {
+        System.out.println("enter make");
+        System.out.println("enter model");
+        System.out.println("enter Storage");
+        Scanner sc= new Scanner(System.in);
+    }
+}
